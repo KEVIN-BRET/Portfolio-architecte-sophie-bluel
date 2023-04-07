891b3b802dedab590ad60651e79dbcac3ae6a958
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-04-07 20:00:20 +0200

    changement d'architecture index.js

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,28 +1,33 @@
-let works = []; // stock les projets de ./api/works
+// on pointe la balise dans laquelle va s'afficher les "projets"
+const gallery = document.querySelector(".gallery");
 
-// génèrer le contenu de la galerie
-function generateWorksInGallery() {
-  fetch("http://localhost:5678/api/works")
+// variable qui va recevoir les données de l'api/works/
+let works = [];
+
+// fonction de récupération des données de l'api/works/
+async function fetchWorks() {
+  await fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
-    .then((data) => {
-      works = data;
-
-      const gallery = document.querySelector(".gallery");
-      for (let i = 0; i < works.length; i++) {
-        const work = works[i];
-        // génération du DOM avec chaque "work"
-        const figure = document.createElement("figure");
-        const imageElement = document.createElement("img");
-        imageElement.src = work.imageUrl;
-        imageElement.alt = work.title;
-        const subtitleElement = document.createElement("figcaption");
-        subtitleElement.innerText = work.title;
-
-        gallery.appendChild(figure);
-        figure.appendChild(imageElement);
-        figure.appendChild(subtitleElement);
-      }
-    });
+    .then((data) => (works = data));
+  // console.log(works);
+  WorksDisplay();
 }
 
-generateWorksInGallery();
+// fonction pour afficher tous les projets dans <gallery> ..
+function WorksDisplay() {
+  gallery.innerHTML = works
+    .map(
+      (work) =>
+        `
+        <figure>
+          <img src="${work.imageUrl}" alt="${work.title}">
+          <figcaption>${work.title}</figcaption>
+        </figure>
+      `
+    )
+    .join("");
+  // console.log(works);
+}
+
+// au chargement de la page, on afffiche tous les projets :
+window.addEventListener("load", fetchWorks);
