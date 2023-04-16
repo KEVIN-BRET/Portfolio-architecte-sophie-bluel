bea21f29082b24853773a4d05f7747824130cf9f
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-04-16 16:28:18 +0200

    ecriture de login.js (wip)

---
+++ FrontEnd/login.js
@@ -0,0 +1,55 @@
+// On pointe les inputs email & password :
+const inputEmail = document.getElementById("email");
+const inputPassword = document.getElementById("userPassword");
+// console.log(loginForm, inputEmail, inputPassword);
+
+// on créé une variable qui va recupérer la valeur (en string) ..
+// de l'email saisit :
+let userEmail = "";
+// et du password saisit :
+let userPassword = "";
+
+// on listen la valeur ecrite dans les inputs email @ password ...
+// on envoi l'email saisi dans la variable userMail :
+inputEmail.addEventListener("input", (e) => {
+  userEmail = e.target.value;
+  console.log(userEmail);
+});
+// on envoi le password saisi dans la variable userPassword :
+inputPassword.addEventListener("input", (e) => {
+  userPassword = e.target.value;
+  console.log(userPassword);
+});
+
+function authentification() {
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: userEmail,
+      password: userPassword,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      localStorage.setItem("token", data.token);
+      window.location.href = "index.html";
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+// On pointe le formulaire :
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", () => {
+  authentification();
+});
+// On pointe le titre du formulaire :
+const loginTitle = document.getElementById("login-title");
+loginTitle.addEventListener("click", () => {
+  authentification();
+});
