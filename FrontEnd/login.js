16e64b75188a62eb268c811a8c36cae798205c82
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-04-17 16:49:38 +0200

    message d'erreur login.js + placeholder input form

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,29 +1,3 @@
-// On pointe le formulaire :
-// const loginForm = document.getElementById("login-form");
-
-// // On pointe les inputs email & password :
-// const inputEmail = document.getElementById("email");
-// const inputPassword = document.getElementById("password");
-// // console.log(loginForm, inputEmail, inputPassword);
-
-// // on créé une variable qui va recupérer la valeur (en string) ..
-// // de l'email saisit :
-// let userEmail = "";
-// // et du password saisit :
-// let userPassword = "";
-
-// // on listen la valeur ecrite dans les inputs email @ password ...
-// // on envoi l'email saisi dans la variable userMail :
-// inputEmail.addEventListener("input", (e) => {
-//   userEmail = e.target.value;
-//   console.log(userEmail);
-// });
-// // on envoi le password saisi dans la variable userPassword :
-// inputPassword.addEventListener("input", (e) => {
-//   userPassword = e.target.value;
-//   console.log(userPassword);
-// });
-
 loginform.addEventListener("submit", (e) => {
   e.preventDefault();
   authentification();
@@ -43,11 +17,17 @@ function authentification() {
   })
     .then((response) => response.json())
     .then((data) => {
-      localStorage.setItem("Sophie Bluel Token", data.token);
-      window.location.href = "index.html";
-      // console.log(data);
+      if (data.token) {
+        loginerror.innerText = "";
+        localStorage.setItem("SophieBluelToken", data.token);
+        // window.location.href = "index.html";
+        console.log(data.token);
+      } else {
+        loginerror.innerText = "email ou mot de passe incorrect !";
+        console.log("email ou mot de passe incorrect !");
+      }
     })
     .catch((error) => {
-      console.log(error);
+      console.log("l'API n'a pas répondue : " + error);
     });
 }
