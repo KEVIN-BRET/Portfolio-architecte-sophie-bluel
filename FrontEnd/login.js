ecf512d810f15504117899c00d0b82bf3920096a
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-04-17 12:31:39 +0200

    refactor de login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,29 +1,30 @@
 // On pointe le formulaire :
-const loginForm = document.getElementById("login-form");
-// On pointe les inputs email & password :
-const inputEmail = document.getElementById("email");
-const inputPassword = document.getElementById("userPassword");
-// console.log(loginForm, inputEmail, inputPassword);
+// const loginForm = document.getElementById("login-form");
 
-// on créé une variable qui va recupérer la valeur (en string) ..
-// de l'email saisit :
-let userEmail = "";
-// et du password saisit :
-let userPassword = "";
+// // On pointe les inputs email & password :
+// const inputEmail = document.getElementById("email");
+// const inputPassword = document.getElementById("password");
+// // console.log(loginForm, inputEmail, inputPassword);
 
-// on listen la valeur ecrite dans les inputs email @ password ...
-// on envoi l'email saisi dans la variable userMail :
-inputEmail.addEventListener("input", (e) => {
-  userEmail = e.target.value;
-  console.log(userEmail);
-});
-// on envoi le password saisi dans la variable userPassword :
-inputPassword.addEventListener("input", (e) => {
-  userPassword = e.target.value;
-  console.log(userPassword);
-});
+// // on créé une variable qui va recupérer la valeur (en string) ..
+// // de l'email saisit :
+// let userEmail = "";
+// // et du password saisit :
+// let userPassword = "";
 
-loginForm.addEventListener("submit", (e) => {
+// // on listen la valeur ecrite dans les inputs email @ password ...
+// // on envoi l'email saisi dans la variable userMail :
+// inputEmail.addEventListener("input", (e) => {
+//   userEmail = e.target.value;
+//   console.log(userEmail);
+// });
+// // on envoi le password saisi dans la variable userPassword :
+// inputPassword.addEventListener("input", (e) => {
+//   userPassword = e.target.value;
+//   console.log(userPassword);
+// });
+
+loginform.addEventListener("submit", (e) => {
   e.preventDefault();
   authentification();
 });
@@ -36,15 +37,15 @@ function authentification() {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      email: userEmail,
-      password: userPassword,
+      email: email.value,
+      password: password.value,
     }),
   })
     .then((response) => response.json())
     .then((data) => {
-      localStorage.setItem("Token Sophie Bluel", data.token);
+      localStorage.setItem("Sophie Bluel Token", data.token);
       window.location.href = "index.html";
-      console.log(data);
+      // console.log(data);
     })
     .catch((error) => {
       console.log(error);
