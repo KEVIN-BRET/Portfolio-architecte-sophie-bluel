13ba7e70b376f033f04db0e757facbb4f9fd304e
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-04-17 20:58:19 +0200

    login.js gestion de la validation des inputs

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,8 +1,4 @@
-loginform.addEventListener("submit", (e) => {
-  e.preventDefault();
-  authentification();
-});
-
+// On envoie une requette "post" à l'api/users/login :
 function authentification() {
   fetch("http://localhost:5678/api/users/login", {
     method: "POST",
@@ -11,18 +7,23 @@ function authentification() {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
+      // On inclu le mail et le password saisi dans le formulaire :
       email: email.value,
       password: password.value,
     }),
   })
     .then((response) => response.json())
     .then((data) => {
+      // Si les données de réponses contiennent un token ...
       if (data.token) {
         loginerror.innerText = "";
+        // .. on l'enregistre dans le localstorage ..
         localStorage.setItem("SophieBluelToken", data.token);
-        // window.location.href = "index.html";
-        console.log(data.token);
+        // .. puis on redirige l'utilisateurs vers la page d'accueil :
+        window.location.href = "index.html";
+        // console.log(data.token);
       } else {
+        // Si les données ne contiennent pas de token, on affiche une erreur :
         loginerror.innerText = "email ou mot de passe incorrect !";
         // console.log("email ou mot de passe incorrect !");
       }
@@ -32,8 +33,15 @@ function authentification() {
     });
 }
 
-//** validation du formaulaire */
+//** GESTION DES EVENEMENTS **//
 
+// A l'envoi du formulaire, on appelle authentification() :
+loginform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  authentification();
+});
+
+// Validation de l'email saisit :
 loginform.addEventListener("input", () => {
   if (!email.validity.valid) {
     emailerror.innerText = "Veuillez saisir une adresse email valide !";
@@ -41,7 +49,7 @@ loginform.addEventListener("input", () => {
     emailerror.innerText = "";
   }
 });
-
+// Validation du mot de passe saisit :
 loginform.addEventListener("input", () => {
   if (!password.validity.valid) {
     passworderror.innerText =
